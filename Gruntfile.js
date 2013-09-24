4bdd8c3dffd318fb268c7ad7b3338aae72d4a7cd
Marko Locher <marko@locher.at>
2013-09-24 11:40:25 +0200

    Added basic grunt tasks to build, uglify and release a new version of the project Added build and release directories to .gitignore

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-compress');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-bump');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            options: {
+                banner: '/* <%= pkg.name %> - version <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n * See <%= pkg.homepage %> for more information! */\n',
+                reoprt: 'gzip',
+                preserveComment: false
+            },
+            compile: {
+                files: {
+                    'build/libcoverage.min.js': ['libcoverage.eowcs.js', 'libcoverage.wcs.js'],
+                }
+            }
+        },
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: ['pkg'],
+                push: false
+            }
+        },
+        clean: {
+            build: ['build/*'],
+            release: ['release/*']
+        },
+        compress: {
+            release: {
+                options: {
+                    archive: 'release/<%= pkg.name %>.v<%= pkg.version %>.zip'
+                },
+                files: [
+                    { expand: true, cwd: 'build', src: ['*.min.js'], dest: '<%= pkg.name %>' },
+                    { expand: true, src: ['README.rst', 'LICENSE'], dest: '<%= pkg.name %>' },
+                ]
+            }
+        }
+    });
+
+    grunt.registerTask('default', ['clean:build', 'uglify'])
+    grunt.registerTask('release', ['clean', 'bump', 'uglify', 'compress:release']);
+}
